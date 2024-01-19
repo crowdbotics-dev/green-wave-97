910d8ce496d14d49e0abe58259b2de5a88693c57
Crowdbotics <engineering@crowdbotics.com>
2024-01-19 09:34:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-wave-97",
+  url: "https://green-wave-97.botics.co",
+  api: "https://green-wave-97.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
